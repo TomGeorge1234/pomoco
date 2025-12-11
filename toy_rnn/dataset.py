d29b17d1c80fae720bd973b996f3d4db1fcd8538
Tom George <tomgeorge1@btinternet.com>
2025-12-11 14:43:54 -0500

    dataset moved to RNN folder, might undo this later

---
+++ toy_rnn/dataset.py
@@ -0,0 +1,43 @@
+import torch
+from typing import Optional
+from synthetic_data.generator import Generator
+from synthetic_data.synthetic_data_generator import generator
+import numpy as np
+
+
+class Dataset(torch.utils.data.IterableDataset):
+
+    def __init__(
+        self,
+        data_generators: List[Generator],
+    ):
+        super().__init__()
+
+        self.num_sets = len(data_generators)
+        self.data_generators = data_generators
+
+        #TODO create a dict storing info for each dataset and a hash to identify them
+
+
+    def __iter__(
+        self,
+    ):
+        while True:
+            # randomly select a dataset
+            dataset_idx = np.random.randint(0, self.num_sets)
+            generator = self.data_generators[dataset_idx]
+
+            time, latents, spike_counts = generator.generate_data()
+            yield {
+                "time": torch.from_numpy(time).float(),
+                "latents": torch.from_numpy(latents).float(),
+                "spike_counts": torch.from_numpy(spike_counts).float(),
+                "dataset_idx": dataset_idx,
+            }
+
+
+
+ 
+
+if __name__ == "__main__":
+    dataset = Dataset()
